8610df88527c85a6672d08ced42347450edfeabf
markandmark1111@gmail.com <markandmark1111@gmail.com>
2021-06-13 09:24:09 +0200

    simple Java program. Same as JavaExample2 but with adding new package contains InputHelper Class.

---
+++ src/javaexample3/JavaExample3.java
@@ -0,0 +1,16 @@
+
+package javaexample3;
+
+import MyUtilities.InputHelper;
+
+
+public class JavaExample3 {
+
+   
+    public static void main(String[] args) {
+        
+        String myInput= InputHelper.GetInput("Enter the input: ");
+        System.out.println("The output is: "+myInput);
+    }
+    
+}
